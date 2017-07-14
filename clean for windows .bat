3770060227a70118622e638841adcc392fbd70fd
jimlee9004 <jimlee900412@gmail.com>
2017-07-14 23:23:18 +0800

    Update clean for windows .bat

--- clean for windows .bat
+++ clean for windows .bat
@@ -1,11 +1,22 @@
 @echo off
-color 0A
+set TempFile_Name=%SystemRoot%\System32\BatTestUACin_SysRt%Random%.batemp
+( echo "BAT Test UAC in Temp" >%TempFile_Name% ) 1>nul 2>nul
+if exist %TempFile_Name% (
+del %TempFile_Name% 1>nul 2>nul
+GOTO menu
+) else (
+GOTO admin
+)
+
+:menu
+CLS
+COLOR 0a
+MODE con: COLS=77 LINES=36
 echo 歡迎使用 Windows小工具
 echo 由Dream Can科技創意綜合研究室製作
-echo .
-echo .
-echo .
-echo .
+echo =============================================================================
 echo 請依照您的需求執行以下功能
-echo 1) 系統清理
-echo 2) 
+echo A) 系統清理
+echo B) 
+echo C)
+echo D) 進入進階模式
