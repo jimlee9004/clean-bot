c890612e9498697edf4537919a889df09f0c6356
jimlee9004 <jimlee900412@gmail.com>
2017-07-15 00:05:06 +0800

    Update clean for windows .bat

--- clean for windows .bat
+++ clean for windows .bat
@@ -25,6 +25,22 @@ if %a%==2 goto Esc
 if %a%==3 goto Esc
 if %a%==4 goto menuPro
 :clean
+echo 正在清除系統垃圾文件，請稍等 ...... 
+del /f /s /q %systemdrive%\*.tmp 
+del /f /s /q %systemdrive%\*._mp 
+del /f /s /q %systemdrive%\*.log 
+del /f /s /q %systemdrive%\*.gid 
+del /f /s /q %systemdrive%\*.chk 
+del /f /s /q %systemdrive%\*.old 
+del /f /s /q %systemdrive%\recycled\*.* 
+del /f /s /q %windir%\*.bak 
+del /f /s /q %windir%\prefetch\*.* 
+rd /s /q %windir%\temp & md %windir%\temp 
+del /f /q %userprofile%\cookies\*.* 
+del /f /q %userprofile%\recent\*.* 
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*" 
+del /f /s /q "%userprofile%\Local Settings\Temp\*.*" 
+del /f /s /q "%userprofile%\recent\*.*" 
 del /s /f /q c:\windows\temp\*.*
 rd /s /q c:\windows\temp
 md c:\windows\temp
@@ -41,4 +57,53 @@ deltree /y c:\windows\cookies
 deltree /y c:\windows\recent
 deltree /y c:\windows\spool\printers
 del c:\WIN386.SWP
-cls 
+del /f /s /q %systemdrive%\*.tmp
+del /f /s /q %systemdrive%\*._mp
+del /f /s /q %systemdrive%\*.log
+del /f /s /q %systemdrive%\*.gid
+del /f /s /q %systemdrive%\*.chk
+del /f /s /q %systemdrive%\*.old
+del /f /s /q %systemdrive%\recycled\*.*
+del /f /s /q %windir%\*.bak
+del /f /s /q %windir%\prefetch\*.*
+del /f /q %userprofile%\cookies\*.*
+del /f /q %userprofile%\recent\*.*
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*"
+del /f /s /q "%userprofile%\Local Settings\Temp\*.*"
+del /f /s /q "%userprofile%\recent\*.*"
+DEL /S /F /Q "%systemroot%\Temp\*.*"
+DEL /S /F /Q "%AllUsersProfile%\「開始」功能表\程式集\Windows Messenger.lnk"
+RD /S /Q %windir%\temp & md %windir%\temp
+RD /S /Q "%userprofile%\Local Settings\Temp"
+MD "%userprofile%\Local Settings\Temp"
+RD /S /Q "%systemdrive%\Program Files\Temp"
+MD "%systemdrive%\Program Files\Temp"
+RD /S /Q "%systemdrive%\d"
+echo清空IE臨時文件目錄... 
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*" 
+del /f /s /q "%userprofile%\Local Settings\Temp\* .*"
+echo正在清除系統臨時文件*.tmp *._tmp *.log *.chk *.old ，請稍等... 
+del /f /s /q %systemdrive%\*.tmp 
+del /f /s /q % systemdrive%\*._mp 
+rem del /f /s /q %systemdrive%\*.log 
+del /f /s /q %systemdrive%\*.gid 
+del /f /s /q %systemdrive%\*.chk 
+del /f /s /q %systemdrive%\*.old
+echo清空垃圾箱，備份文件和預緩存腳本... 
+del /f /s /q %systemdrive%\recycled\*.* 
+del /f /s /q %windir%\*.bak 
+del /f /s / q %windir%\prefetch\*.* 
+rd /s /q %windir%\temp & md %windir%\temp
+rem del /f /q %userprofile%\COOKIES s\*.* 
+rem del /f /q %userprofile%\recent\*.*
+echo清理系統無用文件... 
+%windir%\system32\sfc.exe /purgecache
+echo優化預讀信息... 
+%windir%\system32\defrag.exe %systemdrive% -b
+echo 清除系統垃圾檔案完成！
+echo 按任意見返回簡易選單
+pause>nul
+goto menuEZ
+:Esc
+exit
+:menuPro
